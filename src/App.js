af392c8f7c22a88ff2a9bb91ffab5edbf80e8d2d
anjaliasha123 <anjaliasha95@gmail.com>
2022-11-11 09:37:39 -0600

    props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import './App.css';
 import ExpenseItems from './components/ExpenseItems';
 
 function App() {
+  var expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2021, 4, 17),
+    },
+     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+     {
+       id: 'e3',
+       title: 'Car Insurance',
+       amount: 294.67,
+       date: new Date(2021, 2, 28),
+     },
+     {
+       id: 'e4',
+       title: 'New Desk (Wooden)',
+       amount: 450,
+       date: new Date(2021, 5, 12),
+     },
+  ]
   return (
     <div className="App">
       <h1>App component</h1>
-      <ExpenseItems></ExpenseItems>
+      {
+        expenses.map((item,index) => (
+          <ExpenseItems key={index}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          ></ExpenseItems>
+        ))
+      }
+
+      console.log(expenses)
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,6 +1,13 @@
-function ExpenseItems(){
+import './ExpenseItems.css';
+
+function ExpenseItems(props){
+    
     return (
-        <p>Expense Items</p>
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'><h2>{props.title}</h2></div>
+            <div className='expense-item__price'>{props.amount}</div>
+        </div>
     );
 }
 export default ExpenseItems;
