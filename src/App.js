d2c3cb7369e5ece621e23ba9a726ba237b0a9c63
anjaliasha123 <anjaliasha95@gmail.com>
2022-11-11 08:08:17 -0600

    adding expense tracker component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ExpenseItems from './components/ExpenseItems';
 
 function App() {
   return (
     <div className="App">
       <h1>App component</h1>
+      <ExpenseItems></ExpenseItems>
     </div>
   );
 }
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,6 @@
+function ExpenseItems(){
+    return (
+        <p>Expense Items</p>
+    );
+}
+export default ExpenseItems;
